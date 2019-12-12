3d98cc26221f9164daf8589139461ca0f5766966
lcuetg00 <lcuetg00@estudiantes.unileon.es>
2019-12-12 19:51:02 +0100

    Cambios en el main.

--- tsunamiDemocratic.c
+++ tsunamiDemocratic.c
@@ -2,41 +2,42 @@
 #include <stdlib.h>
 
 #define MAXUSR 15
-#define MAXPART 4
+#define MAXSOCIALACT 4
 
 
 // - -- --- METODOS --- -- -
 //Manejadoras
-void manejadoraInvitacion();
-void manejadoraQr();
+void manejadoraSolicitud(int sig);
 void manejadoraTerminar();
 
 //Auxiliares
+void writeLogMessage(char *id, char *msg);
+int calculaAleatorios(int min, int max);
 
 
 // - -- --- VARIABLES GLOBALES --- -- -
 
-//Listas de pids
-pid_t *listaUsuarios;
-pid_t *actividadesSociales;
 
 //Semaforo
 pthread_mutex_t semaforo; 
+pthread_mutex_t semLog;  //Mutex que controla la escritura en el log.
+pthread_mutex_t semSolicitudes;   //Mutex que controla la aprobacion o no de las solicitudes.
+pthread_mutex_t semActividadSocial;  //Mutex que controla la entrada a la actividad social.
 
 //Archivo de logs: logFile
 FILE *logFile;
 
 //Contadores
-int contadorSolicitudes;
-int contadorUsuarios;
-int contadorEventos;
+int contadorSolicitudes; //Numero unico para el id de la solicitud
+int contadorUsuarios; //Numero de usuarios en la aplicacion
+int contadorEventos; //Numero de usuarios en un evento
 
 //Struct con los valores necesarios para el usuario
 struct solicitudUsuario{
 	int id;
 	int atendido;
 	int tipo;
-};
+} Usuario;
 
 struct solicitudUsuario *colaSolicitud;
 struct solicitudUsuario *colaEventos;
@@ -45,6 +46,9 @@ struct solicitudUsuario *colaEventos;
 
 
 int main (){
+
+	srand(time(NULL));
+
 	if(signal(SIGUSR1, manejadoraInvitacion)==SIG_ERR){
 		perror("LLamada a manejadora fallida.");
 		exit(-1);
@@ -57,20 +61,46 @@ int main (){
 		perror("LLamada a manejadora fallida.");
 		exit(-1);
 	}
-	
-return 0;
+
+	//Inicializamos las listas
+	struct solicitudUsuario *colaSolicitud = (struct solicitudUsuario*)malloc(MAXUSR*sizeof(struct solicitudUsuario));
+	for(int i=0;i<MAXUSR;i++) {
+		solicitudUsuario[i].id=0;	
+		solicitudUsuario[i].atendidot=0;
+		solicitudUsuario[i].tipo=0
+	}
+	struct solicitudUsuario *colaEventos = (struct solicitudUsuario*)malloc(MAXSOCIALACT*sizeof(struct solicitudUsuario));
+	for(int i=0;i<MAXSOCIALACT;i++) {
+		solicitudUsuario[i].id=0;	
+		solicitudUsuario[i].atendidot=0;
+		solicitudUsuario[i].tipo=0
+	}
+
+	pause(); //Pause para que espere por las senyales
 }
 
-void manejadoraInvitacion(){
+void manejadoraSolicitud(int sig){
 
 }
 
-void manejadoraQr(){
-
-}
 
 void manejadoraTerminar(){
 
+}
+
+void writeLogMessage(char *id, char *msg) { 
+	// Calculamos la hora actual 
+	time_t now = time(0);
+	struct tm *tlocal = localtime(&now); 
+	char stnow[19]; strftime(stnow, 19, "%d/%m/%y %H:%M:%S", tlocal); 
+	// Escribimos en el log 
+	logFile = fopen(logFileName, "a"); 
+	fprintf(logFile, "[%s] %s: %s\n", stnow, id, msg); 
+	fclose(logFile); 
+}
+
+int calculaAleatorios(int min, int max) {
+	return rand() % (max-min+1) + min;
 }
 
 
